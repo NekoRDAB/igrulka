951cbe4d50a05f8b8ca2e59ce6cd3796fbcc5d84
Danil <ferrum268@mail.ru>
2023-04-24 17:26:06 +0500

    added Laser Turet scripts

---
+++ Assets/Scripts/Turrets/LaserTurret.cs
@@ -0,0 +1,50 @@
+using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserTurret : MonoBehaviour
+{
+    [SerializeField] private GameObject laser;
+    public int Damage { get; private set; }
+
+    public float Speed { get; private set; }
+    public int Amount { get; private set; }
+    public float CoolDown { get; private set; }
+
+    private int level;
+    private GameObject ownShip;
+    private PlayerShipBehaviour shipBehaviour;
+    private float elapsed = 0.5f;
+    public float DamageMultiplier { get; set; }
+    public float SpeedMultiplier { get; set; }
+    public int AmountMultiplier { get; set; }
+    public float CoolDownMultiplier { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ownShip = GameObject.Find("OwnShip");
+        shipBehaviour = ownShip.GetComponent<PlayerShipBehaviour>();
+        Amount = 4;
+        CoolDown = 5f;
+        Damage = 50;
+        Speed = 30f;
+        level = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (elapsed >= CoolDown)
+        {
+            for (var i = 0; i < Amount; i++)
+            {
+                var deviation = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+                Instantiate(laser, ownShip.transform.position + deviation, ownShip.transform.rotation);
+            }
+            elapsed = 0;
+        }
+    }
+}
