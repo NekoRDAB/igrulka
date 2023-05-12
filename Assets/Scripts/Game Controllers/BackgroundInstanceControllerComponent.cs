86b59f5641d2ac391ca6804ef73e233c73729f09
mgmman <115487609+mgmman@users.noreply.github.com>
2023-05-12 23:07:50 +0500

    guess what? yeah fixes

--- Assets/Scripts/Game Controllers/BackgroundInstanceControllerComponent.cs
+++ Assets/Scripts/Game Controllers/BackgroundInstanceControllerComponent.cs
@@ -8,6 +8,16 @@ public class BackgroundInstanceControllerComponent : MonoBehaviour
     private AudioSource audio;
     void Start()
     {
+        var obj = GameObject.FindWithTag("music");
+        if (obj != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            gameObject.tag = "music";
+        }
+        DontDestroyOnLoad(gameObject);
         if (!PlayerPrefs.HasKey("musicVolume"))
             PlayerPrefs.SetFloat("musicVolume", 1);
         audio = GetComponent<AudioSource>();
