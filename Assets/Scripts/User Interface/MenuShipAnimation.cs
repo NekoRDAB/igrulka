a5065bf977d20d40e9e7452289a2212c2969bda0
Gurikov Maksim <maximgurikoff@gmail.com>
2023-05-21 14:44:39 +0500

    finished main menu animation

--- Assets/Scripts/User Interface/MenuShipAnimation.cs
+++ Assets/Scripts/User Interface/MenuShipAnimation.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class MenuShipAnimation : MonoBehaviour
 {
+    [SerializeField] private GameObject top;
+    [SerializeField] private GameObject stop;
+    [SerializeField] private GameObject flare;
     private Rigidbody2D rb;
     private AudioSource audioSource;
-    [SerializeField] private Camera camera;
-    [SerializeField] private GameObject top;
+    private bool isStopped;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = transform.up * 50;
+        rb.velocity = transform.up * 200;
         audioSource = GetComponent<AudioSource>();
-        audioSource.volume = PlayerPrefs.GetFloat("soundVolume");
+        if(!PlayerPrefs.HasKey("soundVolume"))
+            PlayerPrefs.SetFloat("soundVolume", 1);
+        audioSource.volume = PlayerPrefs.GetFloat("soundVolume") * 0.7f;
     }
 
     // Update is called once per frame
     void Update()
     {
         var topPos = top.transform.position.y;
-        if (topPos <= camera.rect.yMin)
+        if (topPos >= stop.transform.position.y && !isStopped)
+        {
+            audioSource.Play();
             rb.velocity = Vector2.zero;
+            var prevScale = flare.transform.localScale;
+            flare.transform.localScale = new Vector3(prevScale.x * 3, prevScale.y, prevScale.z);
+            isStopped = true;
+        }
     }
 }
