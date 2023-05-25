b3d387806f872c62d27fb58354cba73b44d19710
Gurikov Maksim <maximgurikoff@gmail.com>
2023-05-25 12:23:39 +0500

    fixed everything

--- Assets/Scripts/User Interface/HealthBar.cs
+++ Assets/Scripts/User Interface/HealthBar.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {    
     [SerializeField] private GameObject ownShip;
-    private Transform healthBar;
+    [SerializeField] private Transform healthBar;
     
     void Start()
     {
-        healthBar = GetComponent<Transform>();
+        
     }
     
     void Update()
     {
+        var shipPosition = ownShip.transform.position;
+        transform.position = new Vector3(shipPosition.x, shipPosition.y - 24, 0);
         var health = ownShip.GetComponent<PlayerShipBehaviour>().health;
         healthBar.localScale = new Vector3((float)(health / 100.0), 1, 1);
     }
