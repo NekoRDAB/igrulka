a37ab946895c73635068a034369778b9edf58961
hempi <115639626+VladislavBukirev@users.noreply.github.com>
2023-05-25 21:52:41 +0500

    change maxlevel

--- Assets/Scripts/Game Controllers/GameStateController.cs
+++ Assets/Scripts/Game Controllers/GameStateController.cs
@@ -62,7 +62,7 @@ public class GameStateController : MonoBehaviour
         {
             level++;
             experience = 0;
-            if (level <= levelUpScreen.turrets.Count * 4)
+            if (level <= 20)
                 LevelUp();
         }
     }
