b7b81fc0450099bb397cc92c8226aa11398c5908
Gurikov Maksim <maximgurikoff@gmail.com>
2023-05-25 12:07:27 +0500

    added new heatlh bar and some other fixes

--- Assets/Scripts/User Interface/HealthBar.cs
+++ Assets/Scripts/User Interface/HealthBar.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {    
     [SerializeField] private GameObject ownShip;
-    private Transform healthBar;
+    [SerializeField] private Transform healthBar;
     
     void Start()
     {
-        healthBar = GetComponent<Transform>();
+        
     }
     
     void Update()
     {
+        var ownShipPosition = ownShip.transform.position;
+        transform.position = new Vector3(ownShipPosition.x, ownShipPosition.y - 26, 0);
         var health = ownShip.GetComponent<PlayerShipBehaviour>().health;
         healthBar.localScale = new Vector3((float)(health / 100.0), 1, 1);
     }
